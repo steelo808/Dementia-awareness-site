29786da20aae98fdeb239ec7cd1005b508055206
Sterling Newsom <sterlingnewsom808@gmail.com>
2023-08-21 02:21:44 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import DrawerAppBar from './components/DrawerAppBar';
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DrawerAppBar from './DrawerAppBar';
+import videoBg from '../assets/dementia.mp4';
+import Stack from '@mui/material/Stack';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <DrawerAppBar />
+      <video src = {videoBg}  autoPlay loop muted />
+      <Stack direction= "column" justifyContent="center" alignItems="center" spacing={1} className="content">
+        <p>Lets put an end to Dementia</p>
+        <h1>Education, Prevention and Awareness for dementia.</h1>
+        </Stack>
+    </div>
+  );
+}
+
+export default Header;
